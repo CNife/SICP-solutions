dfed8625780ca11926bb4cc9c84a18e3ccd9784a
CNife <CNife@vip.qq.com>
2020-08-17 13:19:37 +0800

    🔨 添加1.16的代码文件

--- code/1/1.2/1.16.scm
+++ code/1/1.2/1.16.scm
@@ -1,14 +1,7 @@
-# 练习 1.16
+#lang sicp
 
-定义一个过程，使用求平方操作的迭代过程实现求幂过程。
-
----
-
-[1.16.scm](../../code/1/1.2/1.16.scm)
-
-```Scheme
 (define (my-expt x n)
-  
+
   (define (my-expt-iter x n prev-x)
     (if (= n 0)
         prev-x
@@ -21,10 +14,9 @@
            (- n 1)
            n)
        2))
-  
+
   (cond ((= x 0) 0)
         ((= x 1) 1)
         (else (my-expt-iter x n 1))))
 
 (my-expt 2 10) ; 1024
-```
